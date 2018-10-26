d5a48d45f796fe6608ed33c7e43a5d71338c18d6
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-10-26 13:11:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice()
+  return array.slice()
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
